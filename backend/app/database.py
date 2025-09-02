747e9c7225ab8b5829c1e7e4f155150e0f8ffd8b
20AbhishekRoka02 <75659036+20AbhishekRoka02@users.noreply.github.com>
2025-09-02 16:23:08 +0530

    Fix unique email and user id issue using mariadb. Duplicacy issuse if removed from email.

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,8 +1,14 @@
 from sqlmodel import create_engine
 
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
-sqlite_file_name = "schoolerp.db"
-sqlite_url = f"sqlite:///./{sqlite_file_name}"
+DATABASE_URL=f"mysql+pymysql://{os.environ['MYSQL_USER']}:{os.environ['MYSQL_PASSWORD']}@placement_portal_db:3306/{os.environ['MYSQL_DATABASE']}"
 
-engine = create_engine(sqlite_url, echo=True)
+
+# sqlite_file_name = "schoolerp.db"
+# sqlite_url = f"sqlite:///./{sqlite_file_name}"
+
+engine = create_engine(DATABASE_URL, echo=True)
 # SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- backend/app/models.py
+++ backend/app/models.py
@@ -2,59 +2,60 @@ from sqlmodel import Field, SQLModel, Relationship
 from pydantic import EmailStr
 from datetime import datetime
 
+import uuid
 
 # Creating User models
 
 # Admin User model
 class Admin(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
+    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
     name: str
     email: EmailStr = Field(nullable=False, unique=True, primary_key=True)
     phone: str = Field(max_length=15)
     password: str
     created_on: datetime = Field(default_factory=datetime.now)
-    schools : list["School"] = Relationship(back_populates="admin", cascade_delete=True)
+    # schools : list["School"] = Relationship(back_populates="admin", cascade_delete=True)
     # add School, Students and Teacher users created list. CASCADE DELETE
     
 # School User model
-class School(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-    name: str
-    email: EmailStr
-    phone: str = Field(max_length=15)
-    password: str
-    created_on: datetime = Field(default_factory=datetime.now)
-    students: list["Student"] = Relationship(back_populates="school", cascade_delete=True)
-    teachers: list["Teacher"] = Relationship(back_populates="school", cascade_delete=True)
-    admin : Admin = Relationship(back_populates="schools")
-    admin_id: int = Field(default=None, foreign_key="admin.id", ondelete="CASCADE")
+# class School(SQLModel, table=True):
+#     id: int | None = Field(default=None, primary_key=True)
+#     name: str
+#     email: EmailStr = Field(nullable=False, unique=True, primary_key=True)
+#     phone: str = Field(max_length=15)
+#     password: str
+#     created_on: datetime = Field(default_factory=datetime.now)
+#     students: list["Student"] = Relationship(back_populates="school", cascade_delete=True)
+#     teachers: list["Teacher"] = Relationship(back_populates="school", cascade_delete=True)
+#     admin : Admin = Relationship(back_populates="schools")
+#     admin_id: int = Field(default=None, foreign_key="admin.id", ondelete="CASCADE")
     
 
 # Teacher SET NULL on attendance list
 # Teacher User model
-class Teacher(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-    name: str
-    email: EmailStr
-    phone: str = Field(max_length=15)
-    password: str
-    standard : str | None
-    section : str | None
-    created_on: datetime = Field(default_factory=datetime.now)
-    school: School = Relationship(back_populates="teachers")
-    school_id: int = Field(default=None, foreign_key="school.id", ondelete="CASCADE")
+# class Teacher(SQLModel, table=True):
+#     id: int = Field(default=None, primary_key=True)
+#     name: str
+#     email: EmailStr = Field(nullable=False, unique=True, primary_key=True)
+#     phone: str = Field(max_length=15)
+#     password: str
+#     standard : str | None
+#     section : str | None
+#     created_on: datetime = Field(default_factory=datetime.now)
+#     school: School = Relationship(back_populates="teachers")
+#     school_id: int = Field(default=None, foreign_key="school.id", ondelete="CASCADE")
     
     
 
 # Student User Model
-class Student(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-    name: str
-    email: EmailStr
-    phone: str = Field(max_length=15)
-    password: str
-    standard : str | None
-    section : str | None
-    created_on: datetime = Field(default_factory=datetime.now)
-    school: School = Relationship(back_populates="students")
-    school_id: int = Field(default=None, foreign_key="school.id", ondelete="CASCADE")
+# class Student(SQLModel, table=True):
+#     id: int | None = Field(default=None, primary_key=True)
+#     name: str
+#     email: EmailStr = Field(nullable=False, unique=True, primary_key=True)
+#     phone: str = Field(max_length=15)
+#     password: str
+#     standard : str | None
+#     section : str | None
+#     created_on: datetime = Field(default_factory=datetime.now)
+#     school: School = Relationship(back_populates="students")
+#     school_id: int = Field(default=None, foreign_key="school.id", ondelete="CASCADE")
