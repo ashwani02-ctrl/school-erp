878ca810a64743e432ac12b8c1e501234395bebb
20AbhishekRoka02 <75659036+20AbhishekRoka02@users.noreply.github.com>
2026-01-18 21:06:55 +0530

    Configure redirection of / to /dashboard normally and /auth/* to /dashboard, if token is there, in middleware.ts

--- frontend/webui/middleware.ts
+++ frontend/webui/middleware.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
+import { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
   const token = request.cookies.get("token");
@@ -7,6 +7,15 @@ export function middleware(request: NextRequest) {
   if (!token && request.nextUrl.pathname.startsWith("/dashboard")) {
     return NextResponse.redirect(new URL("/auth/login", request.url));
   }
+
+  // if token is present, and user try to access any page of path /auth/*, redirect user to the dashboard
+  if (token && request.nextUrl.pathname.startsWith("/auth")) {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
+
+  // redirect to /dashboard, if user request for / 
+  if (request.nextUrl.pathname == "/") { return NextResponse.redirect(new URL("/dashboard", request.url))}
+
 
   return NextResponse.next();
 }
